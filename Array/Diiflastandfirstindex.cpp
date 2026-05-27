9deba0edbc86a1df1eba1bcbc5389c471c58fd1d
Utsav Parmar <parmarutsav57@gmail.com>
2026-05-27 21:20:35 +0530

    First commit

---
+++ Array/Diiflastandfirstindex.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {2, 0, 6, 8, 10};
+    int n = 5;
+
+    int diff = arr[n - 1] - arr[0];
+
+    cout<< diff;
+
+    return 0;
+}
